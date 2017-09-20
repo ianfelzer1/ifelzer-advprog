a3248d6942dcad101a68213ec26561c083061284
Programming <programming@USProgramming1.local>
2017-09-20 14:03:58 -0400

    message

---
+++ ianfelzer/Dictionary.py
@@ -0,0 +1,11 @@
+eng2sp = {}
+eng2sp['one'] = 'uno'
+eng2sp['two'] = 'dos'
+eng2sp['three'] = 'tres'
+eng2sp['four'] = "cuatro"
+print eng2sp
+
+print eng2sp['one']
+print eng2sp['two']
+print eng2sp['three']
+print eng2sp['four']
---
+++ ianfelzer/Euler2.py
@@ -0,0 +1,22 @@
+
+def pal(n):
+  ls = str(n)
+  r = int(len(ls)/2)
+
+  for x in range(1,r+1):
+    if ls[x-1] != ls[len(ls)-x]:
+      return False
+  return True
+
+highpal=0
+
+for i in range(100, 999):
+  for j in range (100,999):
+    num = i * j
+    if pal(num) == True and num > highpal:
+      highpal = num
+      print(highpal)
+
+#done 9/18/17
+
+#checked 9/18/17
