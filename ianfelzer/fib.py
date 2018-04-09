0bace32b3004caa4716fd536df2712e3e291db73
ianfelzer1 <31860652+ianfelzer1@users.noreply.github.com>
2018-04-09 14:16:22 -0400

    Update fib.py

--- ianfelzer/fib.py
+++ ianfelzer/fib.py
@@ -1,18 +1,10 @@
-## 0 , 1 , 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
 
-if digit == 0:
-fib = 0
-
-if digit == 1:
-fib = 1
-
-
-else fib = (dig-1) + (dig-2)
-
-
-
-
-
-
+   
+  ## Updated 4/9/18 
 
    
