8f81f52bf8405038aebff47443e1cf5b7cb8dbd5
ianfelzer1 <31860652+ianfelzer1@users.noreply.github.com>
2018-04-09 13:49:50 -0400

    Add files via upload

---
+++ ianfelzer/background.jpg.py
@@ -0,0 +1,11 @@
+import turtle               # allows us to use the turtles library
+
+window = turtle.Screen()        # creates a graphics window
+
+ian = turtle.Turtle()      # create a turtle named nick
+ian.forward(150)           # tell nick to move forward by 150 units
+ian.right(180)
+ian.forward(300)
+
+
+window.exitonclick()
